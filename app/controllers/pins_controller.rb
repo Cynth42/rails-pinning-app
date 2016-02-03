faff1517d7d731340bf2fcb1a801453211206be7
cynthia clinton <cynthiaclinton@cynthias-MBP.fios-router.home>
2016-02-03 17:30:31 -0500

    All the tests passed

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -13,10 +13,17 @@ class PinsController < ApplicationController
       @pin = Pin.find_by_slug(params[:slug])
       render :show
   end
-  
+
+# GET /people/new
   def new
       @pin = Pin.new
   end
+  
+# GET /people/1/edit
+  def edit
+    
+  end
+
   
   def create
       @pin = Pin.create(pin_params)
